4501c66552aab4a471d19c95aa65f8d263178581
KaiHofstetter <kai.hofstetter@gmx.de>
2015-03-09 20:32:00 +0100

    Changed the direction of the symbolic links, since the host cannot follow symbolic links in the container.

--- run_wordpress_xdebug.sh
+++ run_wordpress_xdebug.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if [ -d /wordpress_sources ]; then
-    echo "=> Linking WordPress sources to /wordpress_sources"
-    ln -sf /var/www/html/* /wordpress_sources
+    echo "=> Moving WordPress sources to /wordpress_sources"
+    cp -r /var/www/html/* /wordpress_sources
+    rm -r /var/www/html/*
+    ln -sf /wordpress_sources/* /var/www/html
 fi
 
 ./run.sh
