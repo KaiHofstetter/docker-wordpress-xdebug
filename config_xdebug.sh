35e3a88161e5e510cc4a5114f914706844ee0824
KaiHofstetter <kai.hofstetter@gmx.de>
2015-03-10 17:10:45 +0100

    Some minor refactorings.

--- config_xdebug.sh
+++ config_xdebug.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f /xdebug_configured ]; then
     echo "=> Xdebug is not configured yet, configuring Xdebug ..."
-    DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}'); \
-    echo "xdebug.remote_enable=on" >> /etc/php5/mods-available/xdebug.ini; \
+    DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
+    echo "xdebug.remote_enable=on" >> /etc/php5/mods-available/xdebug.ini
     echo "xdebug.remote_host=$DOCKER_HOST_IP" >> /etc/php5/mods-available/xdebug.ini
     echo "xdebug.remote_port=$XDEBUG_PORT" >> /etc/php5/mods-available/xdebug.ini
 
