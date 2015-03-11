d6fe91fb88dbeebd136ebd9e44b55048e29ffcc7
KaiHofstetter <kai.hofstetter@gmx.de>
2015-03-11 21:12:57 +0100

    Automatic deletion of the mounted host dir content removed.

--- run_wordpress_xdebug.sh
+++ run_wordpress_xdebug.sh
@@ -5,11 +5,10 @@
 if [ -d /wordpress_sources ] && [ ! -f /wordpress_sources_moved ]; then
 
     echo "=> Moving WordPress sources to /wordpress_sources"
-    rm -r /wordpress_sources/*
     cp -rp /var/www/html/* /wordpress_sources
     rm -r /var/www/html/*
     ln -sf /wordpress_sources/* /var/www/html
-    
+
     touch /wordpress_sources_moved
 fi
 
