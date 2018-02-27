7877eca41a16ebf8af2dd412131368af7d3090a4
Eugene Glotov <kivagant@gmail.com>
2018-02-27 19:50:25 +0200

    Configure more xDebug settings
    - Do not use `remote_autostart` together with remote_host
    - Allow to set `XDEBUG_HOST` from environment
    - Add `remote_key` option

--- config_xdebug.sh
+++ config_xdebug.sh
@@ -2,10 +2,32 @@
 
 if [ ! -f /xdebug_configured ]; then
     echo "=> Xdebug is not configured yet, configuring Xdebug ..."
-    DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
-    echo "xdebug.remote_enable=on" >> /etc/php5/mods-available/xdebug.ini
-    echo "xdebug.remote_host=$DOCKER_HOST_IP" >> /etc/php5/mods-available/xdebug.ini
-    echo "xdebug.remote_port=$XDEBUG_PORT" >> /etc/php5/mods-available/xdebug.ini
+    echo "xdebug.remote_enable=on" >> /etc/php/5.6/mods-available/xdebug.ini
+
+    if [ -z $XDEBUG_REMOTE_AUTOSTART ]; then
+        XDEBUG_REMOTE_AUTOSTART="on"
+    fi
+    echo "xdebug.remote_autostart=$XDEBUG_REMOTE_AUTOSTART" >> /etc/php/5.6/mods-available/xdebug.ini
+
+    # https://xdebug.org/docs/all_settings#remote_connect_back
+    # 1. If enabled, the xdebug.remote_host setting is ignored
+    # 2. Doesn't work correctly if Docker for Mac is used
+    if [ -z $XDEBUG_REMOTE_CONNECT_BACK ] ; then
+        XDEBUG_REMOTE_CONNECT_BACK="off"
+    fi
+    echo "xdebug.remote_connect_back=$XDEBUG_REMOTE_CONNECT_BACK" >> /etc/php/5.6/mods-available/xdebug.ini
+
+    if [ "$XDEBUG_REMOTE_CONNECT_BACK" != "on" ] && [ "$XDEBUG_REMOTE_CONNECT_BACK" != "On" ]; then
+        if [ -z $XDEBUG_HOST ] ; then
+            XDEBUG_HOST=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
+        fi
+        echo "xdebug.remote_host=$XDEBUG_HOST" >> /etc/php/5.6/mods-available/xdebug.ini
+        echo "xdebug.remote_port=$XDEBUG_PORT" >> /etc/php/5.6/mods-available/xdebug.ini
+    fi
+
+    if ! [ -z $XDEBUG_REMOTE_KEY ] ; then
+        echo "xdebug.xdebug.remote_key=$XDEBUG_REMOTE_KEY" >> /etc/php/5.6/mods-available/xdebug.ini
+    fi
 
     touch /xdebug_configured
 else
